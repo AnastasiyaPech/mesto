740315ccea80a7104a1885c795e9dfc9e12c47b5
Anastasiya Pechnikova <anastasiyaverty@yandex.ru>
2023-01-20 23:02:09 +0300

    add js code

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
 let popupContainer;
-let btnOpen; 
+let btnOpen;
 let btnClose;
+let formElement;
+let nameInput;
+let jobInput;
+let profileNameInput;
+let profileJobInput;
+
 
 window.onload = function () {
     popupContainer = document.querySelector(".popup");
     btnOpen = document.querySelector(".profile__button");
     btnClose = document.querySelector(".popup__button-exit");
+    formElement = document.querySelector(".popup__form");
+    nameInput = document.querySelector(".popup__form-input_type_firstname");
+    jobInput = document.querySelector(".popup__form-input_type_proffesion");
+    profileNameInput = document.querySelector(".profile__title");
+    profileJobInput = document.querySelector(".profile__text");
+
 
     btnOpen.addEventListener("click", openPopup);
     btnClose.addEventListener("click", closePopup);
+    formElement.addEventListener("submit", handleFormSubmit);
 }
 
 function openPopup() {
     popupContainer.classList.add("popup_opened");
+    nameInput.value = profileNameInput.textContent;
+    jobInput.value = profileJobInput.textContent;
+
+
 }
 
 function closePopup() {
     popupContainer.classList.remove("popup_opened");
 }
+
+
+function handleFormSubmit(evt) {
+    console.log("Starting handleFormSubmit");
+    evt.preventDefault(); 
+    
+
+
+    profileNameInput.textContent = nameInput.value;
+    profileJobInput.textContent = jobInput.value;
+
+    console.log(nameInput.value);
+    console.log(jobInput.value);
+
+  
+    closePopup();
+}
+
+
+
